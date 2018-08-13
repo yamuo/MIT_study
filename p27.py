dd90c738a49d855a0dc90a6a4e26ad28236a203e
山下　彰太 <s-yamashita@MacBook-2.local>
2018-08-13 15:00:34 +0900

    commit my 27

--- p27.py
+++ p27.py
@@ -1,1 +1,21 @@
 x = int(input("Enter an integer:"))
+root = 1
+pwr = 1
+match = 0
+
+for i in range(2,6):
+    pwr = i
+    root = 1
+    while x > root**pwr:
+        if root**pwr == x:
+            break
+        root += 1
+    
+    if root**pwr == x:
+        print("root:",root,"pwe:",pwr)
+        match += 1
+    elif pwr == 5 and match == 0:
+        print("nothing")
+        print(match)
+
+print("total matches:",match)
